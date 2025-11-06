1e37bf6dc0d4ae3ecf31b1b37efd4d5dab1f35b6
zhyre <zhyrecoritico@gmail.com>
2025-11-06 08:55:59 +0800

    setup pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import AboutUs from "./pages/AboutUs/AboutUs";
+import MiniGame from "./pages/MiniGame/MiniGame";
+import Loveletter from "./pages/Loveletter/Loveletter";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hi baby</h1>
-      </header>
-    </div>
+    <Router>
+      <nav className="nav-bar">
+        <Link to="/aboutus">Our Story</Link>
+        <Link to="/minigame">Mini Game</Link>
+        <Link to="/loveletter">Love Letter</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/minigame" element={<MiniGame />} />
+        <Route path="/loveletter" element={<Loveletter />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Loveletter/Loveletter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Loveletter.css";
+
+function Loveletter() {
+  return (
+    <div className="love-container">
+      <h1>💌 My Love Letter for You 💌</h1>
+      <p>Click to reveal each message...</p>
+    </div>
+  );
+}
+
+export default Loveletter;
