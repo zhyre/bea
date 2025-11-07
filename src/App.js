ed26602faa7fef45f8ad628c9a16a05444455837
zhyre <zhyrecoritico@gmail.com>
2025-11-07 18:41:12 +0800

    Fix router basename for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/aboutus" element={<AboutUs />} />
